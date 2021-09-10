c9ad3421bc479f5013603f55ec528436547d0ff6
Grabthesky <christiangp46@gmail.com>
2021-09-10 12:25:11 +0200

    tercer/2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-package main;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello world java 02!");
+		System.out.println("Hello world java 03!");
 		
 	}
 
